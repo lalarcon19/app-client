ff6f3b00973dbb7bfa3206b08020f0bf3593b094
lalarcon19 <128664979+lalarcon19@users.noreply.github.com>
2023-06-07 18:04:30 -0500

    Add files via upload

--- src/main/java/com/credibanco/client/Consumir/HttpService.java
+++ src/main/java/com/credibanco/client/Consumir/HttpService.java
@@ -2,30 +2,34 @@ package com.credibanco.client.Consumir;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.credibanco.client.DTO.UserDto;
+
+import static com.credibanco.dependencia.Http.HttpService.getHttpEntity;
+import com.credibanco.dependencia.dto.UserDTORequest;
+import com.credibanco.dependencia.dto.UserDTOResponse;
 
 public class HttpService {
 	@Value("${variable}")
 	String variable;
+	
+	@Autowired
+	private static UserDTORequest userDTORequest;
 
 	private static final Logger logger = LoggerFactory.getLogger(HttpService.class);
 
 	public static String consumirService() {
-		UserDto userDTO = new UserDto();
-
-		userDTO.setId(2);
-		userDTO.setPassword("12344");
-		userDTO.setUserName("juan");
+		UserDTOResponse userDTOResponse = new UserDTOResponse();
+		userDTOResponse.setId(55L);
+		userDTOResponse.setPassword("17868");
+		userDTOResponse.setUserName("mariana");
 		ResponseEntity<String> response = new RestTemplate().exchange("http://localhost:8081/users/create",
-				HttpMethod.POST, getHttpEntity(userDTO), String.class);
+				HttpMethod.POST, getHttpEntity(userDTORequest), String.class);
 
 		if (response.getStatusCode() == HttpStatus.OK) {
 
@@ -36,17 +40,6 @@ public class HttpService {
 
 	}
 
-	private static HttpEntity getHttpEntity(UserDto userDTO) {
-		return new HttpEntity(userDTO, getHttpHeadersJson());
-	}
 
-	private static HttpHeaders getHttpHeadersJson() {
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/json");
-		headers.add("Accept", "application/json");
-
-		return headers;
-	}
 
 }
